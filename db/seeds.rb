3647cc218b24b69bfea219505b4dd6b50200f744
Alexandre Ferreira <alexvirtualbr@gmail.com>
2020-03-12 01:27:37 -0300

    adicionado algumas trips para iniciar os testes

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,12 @@ end
     password_confirmation: 123456
   )
 end
+
+trip_names = ["Romantic Spots for Honeymoons", 
+                "Top Island Getaways", 
+                "Most Romantic Destinations", 
+                "Top Spots for Skiing" ]
+                
+trip_names.each do |trip_name|
+  Trip.create(name: trip_name)
+end
