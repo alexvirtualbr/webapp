7634362a8fff8ba99b9b71b98931496107f84c84
Alexandre Ferreira <alexvirtualbr@gmail.com>
2020-03-12 00:32:22 -0300

    criado recurso landing

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,9 +1,9 @@
 class LandingController < ApplicationController
-  before_action :authenticate_user!
 
-  layout "landing_layout"
+  #layout "landing_layout"
 
   def index
     @message = Message.new
+    @messages = Message.all
   end
 end
