bf681e7ee9acb648ae248dc89b53c98dd85401e6
Alexandre Ferreira <alexvirtualbr@gmail.com>
2020-03-12 00:08:45 -0300

    criado um job para rodar a renderização das mensagens criadas para room

---
+++ app/jobs/room_broadcast_job.rb
@@ -0,0 +1,17 @@
+class RoomBroadcastJob < ApplicationJob
+  queue_as :default
+
+  
+  def perform(message)
+    ActionCable.server.broadcast 'room_BestRoom', message: render_message(message)
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.renderer.render(partial: 'messages/message', 
+      locals: { message: message })
+  end
+
+
+end
